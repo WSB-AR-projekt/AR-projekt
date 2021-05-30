96b15cb86125d92d31aa4a25d59da40c862aae16
Paweł Ślimko <pawel.slimko@wp.pl>
2021-05-30 13:07:18 +0200

    added helpers; modified main files html

---
+++ js/MainGallery.js
@@ -0,0 +1,27 @@
+import { request } from "../helpers/request";
+
+const addGalleryThemes = async () => {
+  const gallerContainer = document.querySelector(
+    ".photo-mainGallery-container"
+  );
+  let galleryThemes = [];
+};
+
+{
+  /* 
+        <div class="photo-mainGallery-logoHolder" id="holder-streetPhoto">
+          <a
+            href="AR_thematicGallery.html"
+            class="photo-mainGallery-links"
+            id="links-streetPhoto"
+          >
+            <img
+              class="photo-mainGallery-logo"
+              id="logo-streetPhoto"
+              decoding="async"
+            />
+          </a>
+          <h3 class="photo-mainGallery-headline">Fotografia uliczna</h3>
+        </div>
+*/
+}
